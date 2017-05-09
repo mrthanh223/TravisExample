4788b0f19f00368b0363cd799e7a828242278932
mrthanh223 <mrthanh223@gmail.com>
2017-05-09 16:41:03 +0700

    Update

--- KidLearning/fastlane/travis.sh
+++ KidLearning/fastlane/travis.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-fastlane test
-exit $?
-fi
-
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 # Travis CI fetches a shallow clone. We use commit count until HEAD for build number. In order to assure that the count is correct we have to unshallow the clone.
 git fetch --unshallow
